c74ec86b00d3d9ff2ade1083209f33239b5d6607
martenno <martenno@local>
2018-10-13 22:09:44 +0200

    Första incheckning av komplett libpower. Bör fungera.

---
+++ src/libpower/libpower.h
@@ -0,0 +1,17 @@
+
+/**
+* libpower.h
+* Mårten Norman 2018-10-10
+* Date: 
+*/
+
+#ifndef LIBPOWER_H_
+#define LIBPOWER_H_
+
+float calc_power_r (float volt,
+		    float resistance);
+
+float calc_power_i (float volt,
+		    float current);
+
+#endif /* LIBPOWER_H_ */

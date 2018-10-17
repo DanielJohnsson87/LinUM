bbc5a4775b5b18876ae80ab1d2e0fa12a49e0068
martenno <martenno@local>
2018-10-17 21:55:14 +0200

    Nu med rätt electrotest.c ---> Finputsat hur siffrorna skrivs och rättat feltänk att ersättningarna blev parallellkopplade om man valde sånt nät

--- src/electrotest.c
+++ src/electrotest.c
@@ -53,7 +53,7 @@ int main (void) {
 
     resistor = calc_resistance(component_count, connection[0], components);
     printf("Ersättningsresistans:\n");
-    print_resistor(resistor);
+    printf("%.1f\n", resistor);
 
     free(components);
     
@@ -61,10 +61,7 @@ int main (void) {
     printf("%.2f W\n", calc_power_r(voltage, resistor));
 
     component_count = e_resistance(resistor, e12_replace);
-    if ( connection[0] == 'S')
-	 printf("Ersättningsresistanser i E12-serien kopplade i serie:\n");
-    else
-	 printf("Ersättningsresistanser i E12-serien kopplade parallellt:\n");
+    printf("Ersättningsresistanser i E12-serien kopplade i serie:\n");
 
     for (int i = 0; i < component_count; i++)
 	 print_resistor( e12_replace[i]);
@@ -73,16 +70,26 @@ int main (void) {
 void print_resistor(float val)
 {
      if (val < 1 )                  /* 0.10 - 0.82 ohm */
-	  printf("%4.2f\n", val);
+	  printf("%.2f\n", val);
      else if (val < 10 )            /* 1.0 - 8.2 ohm */
-	  printf("%3.1f\n", val);
-     else if (val < 100 )           /* 10 - 82 ohm */
-	  printf("%2.0f\n", val);
-     else if (val < 1000 )          /* 100 - 820 ohm */
-	  printf("%3.0f\n", val);
-     else  if (val < 10e9)          /* 1.0e3 - 8.2e9 ohm */
-	  printf("%5.1e\n", val);
-     else                           /* 1.0e10 - 8.2e10 ohm */
+	  printf("%1.1f\n", val);
+     else if (val < 10000 )         /* 10 - 8200 ohm */
+	  printf("%.0f\n", val);
+     else if (val < 100e3 )         /* 10e3 - 82e3 ohm */
+	  printf("%2.0fe3\n", val/1e3);
+     else  if (val < 1000e3)        /* 100e3 - 820e3 ohm */
+	  printf("%3.0fe3\n", val/1e3);
+     else  if (val < 10e6)         /* 1.0e6 - 8.2e6 ohm */
+	  printf("%1.1fe6\n", val/1e6);
+     else  if (val < 100e6)         /* 10e6 - 82e6 ohm */
+	  printf("%2.0fe6\n", val/1e6);
+     else  if (val < 1000e6)        /* 100e6 - 820e6 ohm */
+	  printf("%3.0fe6\n", val/1e6);
+     else  if (val < 10e9)         /* 1.0e9 - 8.2e9 ohm */
+	  printf("%1.1fe9\n", val/1e9);
+     else  if (val < 10e9)         /* 10e9 - 82e9 ohm */
+	  printf("%2.0fe9\n", val/1e9);
+     else                           /* very big */
 	  printf("%6.1e\n", val);
 }
 
